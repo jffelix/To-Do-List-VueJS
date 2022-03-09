13e30c4236043b0449d512f39c3f3abe3da7afd0
Jean-Luc Felix <jean-luc-mac@jean-lucs-mbp.lan>
2022-03-09 13:21:47 -0800

    Added notes

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -14,6 +14,7 @@
 
 import GroceryList from "./components/GroceryList.vue";
 import InputForm from "./components/InputForm.vue";
+// download and import axios here later
 
 export default {
   name: 'App',
