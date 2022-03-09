dbfa1bdbc2d801991ee7e5ac64574e48b9b08420
Jean-Luc Felix <jean-luc-mac@jean-lucs-mbp.lan>
2022-03-08 22:42:17 -0800

    Created onClick handler for Update Item button

--- to-do-list/src/components/GroceryList.vue
+++ to-do-list/src/components/GroceryList.vue
@@ -3,7 +3,7 @@
   <div v-for="grocery in fullList">
       <p>{{grocery.name}}</p>
       <p>{{grocery.quantity}}</p>
-      <button>Update Item</button>
+      <button v-on:click="updateItem()">Update Item</button>
       <button>Delete Item</button>
   </div>
 </template>
@@ -15,6 +15,11 @@ export default {
     // prop type needs to be specified
     props: {
         fullList: Array
+    },
+    methods: {
+        updateItem() {
+            console.log("You clicked Update button!");
+        }
     }
 }
 
