cc597284b6a74ee3ef010486977e9374f7ccc5dd
Jean-Luc Felix <jean-luc-mac@jean-lucs-mbp.lan>
2022-03-08 23:03:45 -0800

    Logged addObj in submitInput

--- to-do-list/src/components/InputForm.vue
+++ to-do-list/src/components/InputForm.vue
@@ -1,9 +1,17 @@
 <template>
   <h3>Add New Item</h3>
   <p>Name</p>
-    <input />
+    <input 
+      type="text" 
+      placeholder="ex: strawberries"
+      v-model="item.name"
+    />
   <p>Quantity</p>
-    <input />
+    <input
+      type="text" 
+      placeholder="ex: 2"
+      v-model="item.quantity"
+    />
   <p></p>
   <button v-on:click="submitInput()">Submit Item</button>
 </template>
@@ -12,9 +20,21 @@
 
 export default {
     name: "InputForm",
+    data() {
+        return {
+            item: {
+                name: null,
+                quantity: null
+            }
+        }
+    },
     methods: {
         submitInput() {
-            console.log("You clicked Submit button!");
+            const addObj = {
+                name: this.item.name,
+                quantity: Number(this.item.quantity)
+            }
+            console.log("addObj: ", addObj);
         }
     }
 }
